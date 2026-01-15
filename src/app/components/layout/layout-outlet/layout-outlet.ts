1a055baf4ffef10b050e62966e3ccd6ca2b08a86
Matthew Layton <matthew.layton@live.co.uk>
2026-01-15 18:49:30 +0000

    Initial commit

---
+++ src/app/components/layout/layout-outlet/layout-outlet.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-layout-outlet',
+  imports: [],
+  templateUrl: './layout-outlet.html',
+  styleUrl: './layout-outlet.scss',
+})
+export class LayoutOutlet {
+
+}
